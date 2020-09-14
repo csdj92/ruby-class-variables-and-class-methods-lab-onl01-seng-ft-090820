381ec882366203821e2642e9d831cada71571e40
CJ Davis <csdj92@gmail.com>
2020-09-14 17:30:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,14 @@
 class Song 
 attr_accessor :artist, :count, :genre 
-@@count
+@@count = 0 
+
+def initialize
+  @@count +=1
+  @@genres = []
+end
+  
+  def self.count
+    @@count
+  end
+ def genres
 end
